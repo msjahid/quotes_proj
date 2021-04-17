484b87fd7b0c0b3fddd085e7a185bc61410b1ebe
msjahid <nirjonjahid@gmail.com>
2021-04-17 20:42:22 +0600

    Quotes POSTGRESQL DATABASE

--- quotes_proj/settings.py
+++ quotes_proj/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'quotes_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quotes',
+        'USER': 'postgres',
+        'PASSWORD': '5271',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
